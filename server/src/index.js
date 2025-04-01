17cce417536c709a7ed24e92e16defa412994219
avfonseca00 <avfonseca00@gmail.com>
2025-04-01 09:45:28 -0400

    fix(server): Rollback the cors config to initial

--- server/src/index.js
+++ server/src/index.js
@@ -3,19 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/mongoDB')
 
-const FRONT_URI = process.env.CLIENT_URI
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
 app.use(express.json())
-app.use(cors({  
-    origin: FRONT_URI,  
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos  
-    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos  
-    credentials: true // Si estás usando cookies, sesión o autorización básica  
-}));
-
-app.options('*', cors());
+app.use(cors());
 
 app.use('/api', require('./routes'))
 
