78b99ffcbbfe1c44c69794f721bfd3af80bf2f45
avfonseca00 <avfonseca00@gmail.com>
2025-04-01 00:50:31 -0400

    fix(server): Fix CORS policy to allow the client access

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/mongoDB')
 
+const FRONT_URI = process.env.CLIENT_URI
+
 const app = express();
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: FRONT_URI
+}));
 
 const PORT = process.env.SERVER_PORT;
 
